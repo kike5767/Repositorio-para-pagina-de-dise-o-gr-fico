35acb4908b9840b0f848d5b3279459eda853f419
quique98495767 <LUISENRIQUEMORALESMONTOYA@GMAIL.com>
2024-06-07 15:07:38 -0500

    Mejora inicial con el archivo RolesController.php original

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -4,133 +4,56 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Role;
-use App\Models\Permission;
-use Illuminate\Support\Facades\DB;
-use App\Models\RolePermission;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;  // Importa la clase Route
 
 class RolesController extends Controller
 {
-    public function index(Request $request)
+    /**
+     * Muestra la vista de inicio de sesión.
+     *
+     * @return View
+     */
+    public function login()
     {
-        if(!empty($request->records_per_page)){
-            $request -> records_per_page = $request -> records_per_page <= env('PAGINATION_MAX_SIZE') ? $request -> records_per_page : env('PAGINATION_MAX_SIZE');
-        }else{
-            $request -> records_per_page = env('PAGINATION_DEFAULT_SIZE');
-        }
-
-        $roles = Role::where('name','LIKE',"%$request->filter%")->paginate($request->records_per_page);;
-
-        return view('roles.index', ['roles'=>$roles, 'data'=>$request]);
+        return view('auth.login', [
+            'canResetPassword' => Route::has('password.request'),
+            'status' => session('status'),
+        ]);
     }
 
-    public function create(){
+    /**
+     * Maneja el inicio de sesión del usuario.
+     *
+     * @param LoginRequest $request
+     * @return RedirectResponse
+     */
+    public function loginPost(LoginRequest $request): RedirectResponse
+    {
+        $request->authenticate();  // Autentica al usuario
 
-        $modules = Permission::all()
-                              ->groupBy('module');
-        return view('roles.create', ['modules'=>$modules]);
+        $request->session()->regenerate();  // Regenera la sesión
+
+        return redirect()->intended(route('welcome', absolute: false));  // Redirige al destino previsto
     }
 
-    public function edit($id){
+    /**
+     * Maneja la salida del usuario.
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function logout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();  // Cierra la sesión del usuario (utiliza Auth aquí)
 
-        $role = Role::find($id);
+        $request->session()->invalidate();  // Invalida la sesión
 
-        if(empty($role)){
-            abort(404, "El role con id '$id' no existe");
-        }
+        $request->session()->regenerateToken();  // Regenera el token de seguridad
 
-        $permissions = Permission::all();
-
-        $permissions = $permissions->map(function($item) use($id){
-            $item->selected = false;
-
-            $rolePermission = RolePermission::where('permission_id', '=', $item->id)
-                                            ->where('role_id', '=', $id)
-                                            ->first();
-
-            if(!empty($rolePermission)){
-                $item->selected = true;
-            }
-            return $item;
-        });
-
-        $modules = $permissions->groupBy('module');
-
-        return view('roles.edit', ['role' => $role, 'modules'=>$modules]);
+        return redirect('/login');  // Redirige a la página de inicio de sesión
     }
-
-    public function delete($id){
-
-        $role = Role::find($id);
-
-        if(empty($role)){
-            abort(404, "El role con id '$id' no existe");
-        }
-
-        $role->delete();
-
-        return redirect()->action([RolesController::class, 'index']);
-    }
-
-
-    public function store(Request $request ){
-        try{
-
-            DB::transaction(function () use ($request){
-                //Creación del rol
-                $role = new Role();
-                $role->name = $request->name;
-                $role->save();
-
-                //Crear la relación y permisos
-                $permissions = json_decode($request->permissions);
-
-                foreach($permissions as $permission){
-
-                    $rolePermission = new RolePermission();
-                    $rolePermission->role_id = $role->id;
-                    $rolePermission->permission_id = $permission;
-                    $rolePermission->save();
-                }
-            });
-
-            return redirect()->action([RolesController::class, 'index']);
-        }catch(Exception $ex){
-            Log::error($ex);
-        }
-    }
-
-    public function update(Request $request ){
-        try{
-            DB::transaction(function() use ($request) {
-
-                //Edición de rol
-                $role = Role::find($request->role_id);
-                $role->name = $request->name;
-                $role->save();
-
-                //Eliminación de permisos viejos
-                RolePermission::where('role_id', '=', $role->id)
-                              ->delete();
-
-                //Crear la relación entre roles y permisos
-                $permissions = json_decode($request->permissions);
-
-                foreach($permissions as $permission){
-
-                    $rolePermission = new RolePermission();
-                    $rolePermission->role_id = $role->id;
-                    $rolePermission->permission_id = $permission;
-                    $rolePermission->save();
-                }
-            });
-
-            return redirect()->action([RolesController::class, 'index']);
-        }catch(Exception $ex){
-            Log::error($ex);
-        }
-    }
-
-
-
 }
