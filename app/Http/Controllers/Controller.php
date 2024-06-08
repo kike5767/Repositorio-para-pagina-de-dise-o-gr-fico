88cc6ae301a8423f3eca5b1d8ceff2660a6d7f06
quique98495767 <LUISENRIQUEMORALESMONTOYA@GMAIL.com>
2024-06-08 04:53:00 -0500

    Mensaje descriptivo de los cambios

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,56 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+class RolesController extends Controller
 {
-    //
+    /**
+     * Muestra la vista de inicio de sesión.
+     *
+     * @return View
+     */
+    public function login(): View
+    {
+        return view('auth.login', [
+            'canResetPassword' => Route::has('password.request'),
+            'status' => session('status'),
+        ]);
+    }
+
+    /**
+     * Maneja el inicio de sesión del usuario.
+     *
+     * @param LoginRequest $request
+     * @return RedirectResponse
+     */
+    public function loginPost(LoginRequest $request): RedirectResponse
+    {
+        $request->authenticate();  // Autentica al usuario
+
+        $request->session()->regenerate();  // Regenera la sesión
+
+        return redirect()->intended(route('welcome', absolute: false));  // Redirige al destino previsto
+    }
+
+    /**
+     * Maneja la salida del usuario.
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function logout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();  // Cierra la sesión del usuario (utiliza Auth aquí)
+
+        $request->session()->invalidate();  // Invalida la sesión
+
+        return redirect('/');  // Redirige a la página de inicio
+    }
 }
